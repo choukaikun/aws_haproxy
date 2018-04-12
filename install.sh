8ba733bf4a2fd4ce8357d18a4d9e0b99878b6ebb
EC2 Default User <ec2-user@ip-172-31-28-219.us-west-2.compute.internal>
2018-04-12 04:55:04 +0000

    Update install.sh and haproxy config file

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
-#!/usr/bin/env 
+#!/usr/bin/env bash
 
 # Install dependant packages
 
+echo "=== Installing dependant packages ==="
 sudo yum install -y gcc openssl-devel systemd-devel
 
+echo "=== Cloning HAProxy GIT repository ==="
 cd /opt
-sudo git clone https://git.haproxy.org.org/git/haproxy-1.8.git/
+sudo git clone https://git.haproxy.org/git/haproxy-1.8.git/
 
+echo "=== Building haproxy binary from source ==="
 cd /opt/haproxy-1.8
 sudo make TARGET=linux2628 USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1
 sudo make install
 
+echo "=== Adding haproxy to systemd ==="
 cd contrib/systemd
 sudo make
 sudo cp haproxy.service /lib/systemd/system
 
+echo "=== Some setup before starting haproxy ==="
+groupadd haproxy
+useradd -g haproxy haproxy
+
 mkdir -p /etc/haproxy
 mkdir -p /var/lib/haproxy
-mkdir /run/haproxy
 
+echo "=== Enabling and starting the haproxy service ==="
 systemctl enable haproxy
 systemctl start haproxy
