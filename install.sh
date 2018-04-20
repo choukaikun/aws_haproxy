eaa389023acb5c8ae1b1c4b418d33b761ef8f644
root <root@ip-172-31-22-153.us-west-2.compute.internal>
2018-04-20 17:18:45 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -4,38 +4,46 @@ SCRIPT_DIR=$(pwd)
 # Install dependant packages
 
 echo "=== Installing dependant packages ==="
-sudo yum install -y gcc openssl-devel systemd-devel
+yum install -y gcc openssl-devel systemd-devel
 
 echo "=== Cloning HAProxy GIT repository ==="
-if [ ! -d /opt/apps ]; then
-  mkdir /opt/apps
+if [ ! -d /opt/apps/unix ]; then
+  mkdir -p /opt/apps/unix
 fi
 
 cd /opt/apps/unix
-sudo git clone https://git.haproxy.org/git/haproxy-1.8.git/
+git clone https://git.haproxy.org/git/haproxy-1.8.git/
 
 echo "=== Building haproxy binary from source ==="
-cd /opt/apps/haproxy-1.8
-#sed -i -e 's/doc\/haproxy/share\/doc\/haproxy/g' Makefile # Fix install-man area
+cd /opt/apps/unix/haproxy-1.8
 
-sudo make TARGET=linux2628 USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1
-#sudo make PREFIX=/usr install
-sudo make install
+make TARGET=linux2628 USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1
+make install
 
 echo "=== Create haproxy user and group ==="
 groupadd -g 65188 haproxy
-useradd -g haproxy -d /var/lib/haproxy -s /sbin/nologin -u 65188 haproxy
+useradd -g haproxy -M -d /var/lib/haproxy -s /sbin/nologin -u 65188 haproxy
 
 echo "=== Adding haproxy to systemd ==="
-sudo cp haproxy.service /lib/systemd/system
+cd /opt/apps/unix/haproxy-1.8/contrib/systemd
+make
+cp haproxy.service /lib/systemd/system
 
 echo "=== Seting up /run/haproxy directory settings ==="
-cp -p ${SCRIPT_DIR}/etc/tmpfiles.d/haproxy /etc/tmpfiles.d
+cp ${SCRIPT_DIR}/etc/tmpfiles.d/haproxy.conf /etc/tmpfiles.d
+systemd-tmpfiles --create
 
 echo "=== Some setup before starting haproxy ==="
 mkdir -p /etc/haproxy
-mkdir -p /var/lib/haproxy
+cp ${SCRIPT_DIR}/etc/haproxy.cfg /etc/haproxy/
+
+mkdir -p /var/lib/haproxy/dev
+cp ${SCRIPT_DIR}/etc/rsyslog.d/49-haproxy.conf /etc/rsyslog.d/
 
 echo "=== Enabling and starting the haproxy service ==="
 systemctl enable haproxy
 systemctl start haproxy
+
+echo "=== Restarting rsyslog ==="
+selinuxenabled && setenforce 0
+systemctl restart rsyslog
